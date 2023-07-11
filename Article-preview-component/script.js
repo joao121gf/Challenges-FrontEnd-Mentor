7c727de267006322fee879ab109b3897b687546a
João Giovani <giovannirs957@gmail.com>
2023-07-10 21:01:19 -0300

    Finalizado

--- Article-preview-component/script.js
+++ Article-preview-component/script.js
@@ -1,4 +1,15 @@
 window.addEventListener("load", () => {
-    const button = document.querySelector('.icon')
-    console.log(button)
+    const button = document.querySelector('.share-icon')
+    const ballon = document.querySelector('.container-contact')
+
+
+    function showPopUp(){
+        ballon.classList.toggle('active')
+        if (ballon.classList.contains('active')){
+            button.classList.add('active-color')
+        }else{
+            ballon.classList.add('desative')
+        }
+    }
+    button.addEventListener('click', showPopUp)
 })
